5f5aac51ac830a1b5c5e4ee0934956d9a7c4a3be
athanas1 <athanas1@L11715042778.wctc.msft>
2016-02-23 16:08:56 -0600

    Discount Strategy -Added Customer, products

--- src/act/discountstrategy/FakeDatabase.java
+++ src/act/discountstrategy/FakeDatabase.java
@@ -16,5 +16,35 @@ public class FakeDatabase {
       new Customer("300","Peter Piper")
     };
     
-    private Product[] products = new Product[3];
+    private Product[] products = {
+    new Product("11","Brewer's Hat", 29.95, new PercentOffDiscount(.10)),
+    new Product("22","Women's Blouse",49.95 ,new FlatAmtDiscount(5.00)),
+    new Product("33","Men's Black Belt",39.95 ,new NoDiscount()),
+    };
+    
+    public final Product findProductById(String prodId){
+          // needs validations
+        Product product = null;
+        
+        for(Product p : products){
+            if(p.getProdId().equals(prodId))
+                product = p;
+                break;
+            }
+        return product;
+    }
+    
+    public final Customer findCustomerById(String custId){
+        // needs validations
+        Customer customer = null;
+        
+        for(Customer c : customers){
+            if(c.getCustId().equals(custId)){
+                customer = c;
+                break;
+            }
+        }
+        
+        return customer;
+    }
 }
